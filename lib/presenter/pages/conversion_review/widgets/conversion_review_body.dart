2cd8c5add0d55db67593afff383cfb46b7f5f027
luan <luan.fortunato@warren.com.br>
2022-10-03 13:59:27 -0300

    ref:aplicando-sugestoes

--- lib/presenter/pages/conversion_review/widgets/conversion_review_body.dart
+++ lib/presenter/pages/conversion_review/widgets/conversion_review_body.dart
@@ -1,8 +1,8 @@
+import 'package:crypto_app/presenter/pages/conversion_review/widgets/row_info_conversion.dart';
 import 'package:flutter/material.dart';
 
 import '../../crypto_info/widgets/divider_crypto_info.dart';
 import '../conversion_review_args.dart';
-import 'row_info_conversion.dart';
 
 class ConversionReviewBody extends StatelessWidget {
   const ConversionReviewBody({
@@ -14,6 +14,18 @@ class ConversionReviewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final converted = args.conversionEntity.convertedCrypto;
+    final toConvert = args.conversionEntity.toConvertCrypto;
+    final String exchValue =
+        (converted.currentPrice.toDouble() / toConvert.currentPrice.toDouble())
+            .toStringAsFixed(2)
+            .replaceAll('.', ',');
+    final String toReceive = '${(args.conversionEntity.quantity * double.parse(
+          converted.currentPrice.toString(),
+        ) / double.parse(
+          toConvert.currentPrice.toString(),
+        )).toStringAsFixed(8).replaceAll('.', ',')} ${toConvert.symbol.toUpperCase()}';
+
     return Column(
       children: [
         const Padding(
@@ -33,18 +45,16 @@ class ConversionReviewBody extends StatelessWidget {
             RowInfoConversion(
               label: 'Converter',
               text:
-                  '${args.conversionEntity.quantity.toStringAsFixed(8).replaceAll('.', ',')} ${args.conversionEntity.convertedCrypto.symbol.toUpperCase()}',
+                  '${args.conversionEntity.quantity.toStringAsFixed(8).replaceAll('.', ',')} ${converted.symbol.toUpperCase()}',
             ),
             const DividerCryptoInfo(),
-            RowInfoConversion(
-                label: 'Receber',
-                text:
-                    '${(args.conversionEntity.quantity * double.parse(args.conversionEntity.convertedCrypto.currentPrice.toString()) / double.parse(args.conversionEntity.toConvertCrypto.currentPrice.toString())).toStringAsFixed(8).replaceAll('.', ',')} ${args.conversionEntity.toConvertCrypto.symbol.toUpperCase()}'),
+            RowInfoConversion(label: 'Receber', text: toReceive),
             const DividerCryptoInfo(),
             RowInfoConversion(
-                label: 'Câmbio',
-                text:
-                    '1 ${args.conversionEntity.convertedCrypto.symbol.toUpperCase()} = ${(args.conversionEntity.convertedCrypto.currentPrice.toDouble() / args.conversionEntity.toConvertCrypto.currentPrice.toDouble()).toStringAsFixed(2).replaceAll('.', ',')} ${args.conversionEntity.toConvertCrypto.symbol.toUpperCase()}'),
+              label: 'Câmbio',
+              text:
+                  '1 $exchValue ${converted.symbol.toUpperCase()} = ${toConvert.symbol}',
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 45),
               child: MaterialButton(
